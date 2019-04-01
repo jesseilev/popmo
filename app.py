c3aec5635458613ca3b67f3ec240491a98f0a3cc
jesseilev <jesseilev@gmail.com>
2019-04-01 14:37:32 -0400

    refactor: move api fetching logic to helper functions

--- app.py
+++ app.py
@@ -4,17 +4,13 @@ from collections import namedtuple
 import re
 
 
-TMDB_URL = 'http://api.themoviedb.org/3/movie/popular'
-TMDB_API_KEY = '9dbd6530be5d59e915f3630acc78409d'
 
-app = Flask(__name__)
+##### MOVIE DATA MODEL #####
 
-@app.route('/')
-def index():
-    return redirect(url_for('movies'))
-
-
-Movie = namedtuple('Movie', ['id', 'title', 'popularity', 'release_date'])
+Movie = namedtuple(
+    'Movie', 
+    ['id', 'title', 'popularity', 'release_date']
+)
 
 def movie_from_json(json_obj):
     return Movie(
@@ -24,27 +20,53 @@ def movie_from_json(json_obj):
         release_date = json_obj['release_date']
     )
 
-@app.route('/movies/')
-def movies():
+
+
+##### TMDB API #####
+
+TMDB_URL = 'http://api.themoviedb.org/3/movie/'
+TMDB_API_KEY = '9dbd6530be5d59e915f3630acc78409d'
+
+def fetch_popular_movies():
+    url = TMDB_URL + 'popular'
     payload = {
         'api_key': TMDB_API_KEY,
         'sort_by': 'popularity.desc'
     }
-    response = requests.get(TMDB_URL, data=payload)
+    response = requests.get(url, data=payload)
     top_ten_movies = [ 
         movie_from_json(json_obj)
         for json_obj in
         response.json()['results'][:10]
     ]
-    return render_template('movies.html', movies=top_ten_movies)
+    return top_ten_movies
 
-@app.route('/movies/<movie_id>')
-def movie(movie_id):
+def fetch_movie(movie_id):
+    url = TMDB_URL + movie_id
     payload = {
         'api_key': TMDB_API_KEY
     }
-    response = requests.get('http://api.themoviedb.org/3/movie/' + movie_id, data=payload)
+    response = requests.get(url, data=payload)
     movie = movie_from_json(response.json())
+    return movie
+
+
+##### APP ######
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('movies'))
+
+@app.route('/movies/')
+def movies():
+    movies = fetch_popular_movies()
+    return render_template('movies.html', movies=movies)
+
+@app.route('/movies/<movie_id>')
+def movie(movie_id):
+    movie = fetch_movie(movie_id)
     return render_template('movie.html', movie=movie)
 
 if (__name__ == '__main__'):
